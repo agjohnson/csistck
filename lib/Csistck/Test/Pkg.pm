65f5b36cb6d8c95482c3ae1c254700b34c89c5e3
Anthony <anthony@ohess.org>
2011-02-08 13:38:52 -0500

    Reverted dpkg test

--- lib/Csistck/Test/Pkg.pm
+++ lib/Csistck/Test/Pkg.pm
@@ -49,7 +49,7 @@ sub pkg_check {
 
     # Decide command, execute. Return MISSING by default
     given ($type) {
-        when ('dpkg') { $cmd = "dpkg -s \"$pkg\""; };
+        when ('dpkg') { $cmd = "dpkg -L \"$pkg\""; };
         when ('pacman') { $cmd = "pacman -Qe \"$pkg\""; };
     }
     
